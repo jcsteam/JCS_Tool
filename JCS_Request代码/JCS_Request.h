5fe57a9ab042bc15cb6464690b0c102c65dccca3
永平 <yongping@anve.com>
2020-03-24 12:41:40 +0800

    目录

--- JCS_Request代码/JCS_Request.h
+++ JCS_Request代码/JCS_Request.h
@@ -15,19 +15,29 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)sharedInstance;
 
++ (instancetype)sharedInstance;
+
 /// 获取BaseUrl
 + (NSString*)getBaseUrl;
-
-/// 对参数进行预处理
+/// 对参数进行预处理，如加密、添加公共参数等，无特殊操作则直接 return params;
 + (NSDictionary*)preprocessParams:(NSDictionary*)params;
-/// 返回data属性
+/// 返回响应中的data属性
+/// 例如服务端响应内容如下
+/// {
+///     code:10000,
+///     msg:"成功",
+///     success:true,
+///     data:{}
+/// }
+/// 则在该方法中 return [requestParams valueForKey:@"data"];
 + (id)dataInResponse:(NSDictionary*)response url:(NSString*)url requestParams:(NSDictionary*)requestParams;
-/// 请求错误
+/// 请求错误时，该方法会被调用，自行处理错误逻辑
 + (void)errorRequest:(NSError*)error url:(NSString*)url requestParams:(NSDictionary*)requestParams;
-
+/// 菊花显示与隐藏
 + (void)showQueryHub;
 + (void)showRequestErrorHub:(NSError*)error;
 + (void)hideRequestHub;
+
 
 @end
 
